397f02ff253f4bebbf1ece9df04876177e1f6faa
mayinot <31657679+mayinot@users.noreply.github.com>
2022-11-16 17:10:14 +0800

    added port

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,7 +3,7 @@
 echo 'The following deploy file will deloy the php application on php:7.2-apache'
 
 set -x
-docker run -d -p 3000:3000 --name my-apache-php-app --volume /var/jenkins_home/workspace/jenkins-php-selenium-test:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app --volume /var/jenkins_home/workspace/jenkins-php-selenium-test:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
