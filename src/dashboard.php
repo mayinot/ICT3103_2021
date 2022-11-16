f3c3b3617a18036288753072c67125d211ce154f
mayinot <31657679+mayinot@users.noreply.github.com>
2022-11-16 18:14:04 +0800

    added a new form with sanitization

--- src/dashboard.php
+++ src/dashboard.php
@@ -6,8 +6,34 @@
 		header('location:index.php');
 		exit;
 	}
+	 
 	
+	if(isset($_POST['submit']))
+	{
+		if((isset($_POST['contactemail']) && $_POST['contactemail'] !='') && (isset($_POST['petname']) && $_POST['petname'] !=''))
+		{
+			$contactemail = trim($_POST['contactemail']);
+			$petname = trim($_POST['petname']);
 
+			 //sanitise string
+			$petname = filter_var($petname, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_STRIP_BACKTICK);
+			
+			if($email == "user@example.com")
+			{	
+				if($petname == "postman cat")
+				{
+					$resultMsg = "Postman cat is requested.";
+					unset($errorMsg);
+					header('location:dashboard.php');
+					exit;
+					
+				}else{
+					$resultMsg = $petname "is requested.";
+				}
+			}
+			$errorMsg = "Submit failed";
+		}
+	}
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +47,39 @@
 	<div class="container-dashboard">
 		Welcome to the dashboard! <span class="user-name"><?php echo ucwords($_SESSION['first_name'])?> <?php echo ucwords($_SESSION['last_name']);?> </span> 
 		<br>
-		
+		<h1> Adopt a pet!</h1> 
+		<?php 
+			if(isset($errorMsg))
+			{
+				echo "<div class='error-msg'>";
+				echo $errorMsg;
+				echo "</div>";
+				unset($errorMsg);
+			}
+			
+			if(isset($resultMsg))
+			{
+				echo "<div class='success-msg'>";
+				echo "You have successfully submitted an adoption request. ";
+				echo $resultMsg;
+				echo "</div>";
+				unset($resultMsg);
+			}
+		?>
+		<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+			<div class="field-container">
+				<label>Pet Name</label>
+				<input type="text" name="petname" required placeholder="Enter Pet Name">
+			</div>
+			<div class="field-container">
+				<label>Contect Email</label>
+				<input type="email" name="contactemail" required placeholder="Enter Your Email">
+			</div>
+			<div class="field-container">
+				<button type="submit" name="submit">Submit</button>
+			</div>
+			
+		</form>
 		<a href="logout.php?logout=true" class="logout-link">Logout</a>
 	</div>
 </body>
